e3c3cf1703a30db736d5ae5ffe31b5412d6cedfc
LukasWeigl <63125168+LukasWeigl@users.noreply.github.com>
2023-04-21 15:25:35 +0200

    added filepicker

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
-const path = require('path');
+const { app, BrowserWindow, ipcMain, dialog } = require("electron");
+const path = require("path");
+const fs = require("fs");
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) {
+if (require("electron-squirrel-startup")) {
   app.quit();
 }
 
@@ -12,39 +13,44 @@ const createWindow = () => {
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
+      preload: path.join(__dirname, "preload.js"),
       nodeIntegration: false,
       contextIsolation: true,
     },
   });
 
   // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+  mainWindow.loadFile(path.join(__dirname, "index.html"));
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
 
-  ipcMain.on('hello-world', (event, arg) => {
-    console.log(arg);
-    event.reply('hello-world-reply', 'Hello from the main process!');
+  ipcMain.on("", (event, arg) => {
+    const filePath = "C:/Users/lukas/OneDrive/Dokumente/testfile.txt"; // Replace with your file path
+    fs.readFile(filePath, "utf8", (err, data) => {
+      if (err) throw err;
+      console.log("Data from IndexJS: " + data);
+    });
+
+    event.reply("hello-world-reply", "Hello from the main process!");
   });
 };
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on("ready", createWindow);
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('activate', () => {
+app.on("activate", () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
