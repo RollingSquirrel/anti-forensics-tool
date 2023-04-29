7fce5258a28a28953ac8b3c4103c88d3cc4b9233
RollingSquirrel <41899314+RollingSquirrel@users.noreply.github.com>
2023-04-29 16:09:33 +0200

    Improved creation time logic and setup prettier

--- src/creation-time-command.js
+++ src/creation-time-command.js
@@ -1,14 +1,14 @@
-const { exec } = require("child_process");
+const { exec } = require('child_process');
 
 async function changeCreationTime(filePath, creationTime, platform) {
   const year = creationTime.getFullYear();
-  const month = String(creationTime.getMonth() + 1).padStart(2, "0");
-  const day = String(creationTime.getDate()).padStart(2, "0");
+  const month = String(creationTime.getMonth() + 1).padStart(2, '0');
+  const day = String(creationTime.getDate()).padStart(2, '0');
 
   const creationTimeFormatted = `${year}-${month}-${day}`;
-  let platformSpecificCommand = "";
+  let platformSpecificCommand = '';
 
-  if (platform == "win32") {
+  if (platform == 'win32') {
     platformSpecificCommand = `powershell.exe -Command "Set-ItemProperty -Path '${filePath}' -Name CreationTime -Value '${creationTimeFormatted}'"`;
   } else {
     platformSpecificCommand = `touch -c -d ${creationTimeFormatted} ${filePath}`;
@@ -16,17 +16,18 @@ async function changeCreationTime(filePath, creationTime, platform) {
 
   await new Promise((resolve, reject) => {
     exec(platformSpecificCommand, (error, stdout, stderr) => {
-      if (error) {
-        console.error(`Error executing command: ${error}`);
-        reject(error);
-      }
-
       if (stderr) {
         console.error(`Error output: ${stderr}\n\n`);
       }
 
       console.log(`Command output: ${stdout}`);
-      resolve(stdout);
+
+      if (error) {
+        console.error(`Error executing command: ${error}`);
+        reject(error);
+      } else {
+        resolve(stdout);
+      }
     });
   });
 }
